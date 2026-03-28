6549776ba7362bccce2274ed183e5bedee5fb9ad
Koushik Kumar <koushikrk1324@gmail.com>
2026-03-29 00:23:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,10 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors({origin:"*"}));
+app.use(cors({
+    origin:"*",
+    credentials: true
+}));
 app.use(express.json());
 
 // Routes
