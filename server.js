17f7db9e4dd6f2767de1dba609a4fb4ff11b4bdf
koushik2411 <koushikrk1324@gmail.com>
2026-03-30 15:02:13 +0530

    Fixed bugs

--- server.js
+++ server.js
@@ -10,7 +10,10 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin:"*",
+    credentials:true
+}));
 app.use(express.json());
 
 // Routes
@@ -23,7 +26,7 @@ app.get("/", (req,res) => {
 });
 
 // MongoBD
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.MONGO_URI)
 .then(() => {
